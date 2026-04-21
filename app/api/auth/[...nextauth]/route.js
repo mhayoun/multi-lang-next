ebebf934dc5391abba34a4956065292adb89d406
Moshe Hayoun <yelotag@gmail.com>
2026-04-21 08:50:25 +0300

    google signe step 1

---
+++ app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,2 @@
+import { handlers } from "@/lib/auth"; // Make sure your auth.ts file is in the root or @/ folder
+export const { GET, POST } = handlers;
---
+++ lib/auth.ts
@@ -0,0 +1,21 @@
+import NextAuth from "next-auth"
+import Google from "next-auth/providers/google"
+import { UpstashRedisAdapter } from "@auth/upstash-redis-adapter"
+import { Redis } from "@upstash/redis"
+
+const redis = new Redis({
+  url: process.env.UPSTASH_REDIS_REST_URL!,
+  token: process.env.UPSTASH_REDIS_REST_TOKEN!,
+})
+
+export const { handlers, auth, signIn, signOut } = NextAuth({
+  adapter: UpstashRedisAdapter(redis),
+  providers: [Google],
+  callbacks: {
+    // Optional: Restrict login to ONLY your email
+    async signIn({ user }) {
+      const allowedEmails = ["your-email@gmail.com"];
+      return allowedEmails.includes(user.email ?? "");
+    },
+  },
+})
